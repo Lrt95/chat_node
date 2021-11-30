8236797e4087cfc980bb3607007e1914bafb848f
Lucas Da Silva Marques <dasilvamarqueslucas@gmail.com>
2021-11-30 14:34:36 +0100

    BDD

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
-
+const dotenv = require('dotenv').config()
 const app = express();
-
 const server = app.listen(3000, () => {
     console.log('server is running on port', server.address().port);
 });
+
+const db = require("./app/models");
+db.mongoose.connect(`mongodb://${process.env.DB_HOSTNAME}:${process.env.DB_PORT}:${process.env.DB_NAME}`)
+    .then(() => {
+        console.log("Connection réussie");
+    })
+    .catch(err => {
+        process.exit();
+    })
